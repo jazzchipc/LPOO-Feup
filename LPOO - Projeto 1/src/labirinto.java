e15b01ebc60c8b9ba54c021a66f58c07c3d6a92f
jazzchipc <josealeixo.pc@outlook.com>
2016-02-24 01:27:06 +0000

    Criei o array com os símbolos que o labirinto deve ter.

--- LPOO - Projeto 1/src/labirinto.java
+++ LPOO - Projeto 1/src/labirinto.java
@@ -1,9 +1,27 @@
 
 public class labirinto {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// array bidimensional para guardar os símbolos do labirinto
+		char[][] symbols = 	
+		{
+				{'X','X','X','X','X','X','X','X','X','X'},
+				{'X','H','0','0','0','0','0','0','0','X'},
+				{'X','0','X','X','0','X','0','X','0','X'},
+				{'X','D','X','X','0','X','0','X','0','X'},
+				{'X','0','X','X','0','X','0','X','0','X'},
+				{'X','0','0','0','0','0','0','X','0','X'},
+				{'X','0','X','X','0','X','0','X','0','X'},
+				{'X','0','X','X','0','X','0','X','0','X'},
+				{'X','E','X','X','0','0','0','0','0','X'},
+				{'X','X','X','X','X','X','X','X','X','X'}
+		};
+		
+		System.out.println(symbols[1][1]);
 
 	}
+	
+	 
 
 }
