b32eca88570fcd223182ac157a72a7e23d2be65a
jazzchipc <josealeixo.pc@outlook.com>
2016-02-24 13:27:54 +0000

    Update ao Jogo.java.

--- LPOO - Projeto 1/src/maze/logic/Jogo.java
+++ LPOO - Projeto 1/src/maze/logic/Jogo.java
@@ -7,14 +7,27 @@ public class Jogo {
 	Labirinto lab = new Labirinto();
 	Heroi heroi = new Heroi();
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
+		Jogo jogo = new Jogo();
+		
+		jogo.initJogo();
+		
+		int i = 0;
+		while (i < 5)
+		{
+		
+		jogo.lab.printLab();
+		
+		jogo.heroi.getPos();
+		
+		}
 	}
 	
 	public void initJogo()
 	{
+		
 	heroi.pos = lab.findPos(heroi.letra); // primeira posição do "H"
-	
-	
+
 	}
 }
