5ebc9d4a0ab71887cdf83abbcdf822277f10e9c0
ASUS <ASUS@ASUSPC>
2016-04-04 10:45:19 +0100

    oi

--- LPOO - Projeto 1/src/maze/test/TestDragon.java
+++ LPOO - Projeto 1/src/maze/test/TestDragon.java
@@ -7,9 +7,38 @@ import maze.logic.*;
 
 public class TestDragon {
 
+	char [][] m1 = {{'X', 'X', 'X', 'X', 'X'},
+					{'X', ' ', ' ', 'H', 'S'},
+					{'X', ' ', 'X', ' ', 'X'},
+					{'X', 'E', ' ', 'D', 'X'},
+					{'X', 'X', 'X', 'X', 'X'}};
+	
 	@Test
 	public void testValidCoords() {
-		fail("Not yet implemented");
+		Game game = new Game(m1);
+		game.initGame('i'); 	// idle dragon
+		Maze maze = game.getMaze();
+		Dragon dragon = game.getDragon();
+		
+		assertTrue(dragon.getPosition().getX() > 0);
+		assertTrue(dragon.getPosition().getY() > 0);
+		assertTrue(dragon.getPosition().getX() < maze.getSize()-1);
+		assertTrue(dragon.getPosition().getY() < maze.getSize()-1);
+		
 	}
 
+	@Test
+	public void testMoveDragonToFreeCell()
+	{
+		Game game = new Game(m1);
+		game.initGame('r'); 	// random dragon
+		Maze maze = game.getMaze();
+		Dragon dragon = game.getDragon();
+		
+		Position p1 = new Position(3,3);
+		
+		assertEquals(maze.charAt(p1), 'D');
+	}
+	
+	
 }
