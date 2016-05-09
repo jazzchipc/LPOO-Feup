a7f37dddea33900cdd2853cb46c0517e314fe270
jazzchipc <josealeixo.pc@outlook.com>
2016-05-09 15:55:33 +0100

    Updated Android Tutorial. Error sending text. But f**k it.

--- TutorialAndroid/app/src/main/java/com/example/josaleixo/tutorialandroid/MainActivity.java
+++ TutorialAndroid/app/src/main/java/com/example/josaleixo/tutorialandroid/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.josaleixo.tutorialandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.josaleixo.tutotialandroid.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
