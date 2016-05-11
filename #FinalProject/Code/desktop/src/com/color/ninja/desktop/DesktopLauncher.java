1ae09eec9e23c33eef88742366cb1943f6e8cd84
jazzchipc <josealeixo.pc@outlook.com>
2016-05-11 11:09:07 +0100

    Added Code folder.

---
+++ #FinalProject/Code/desktop/src/com/color/ninja/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.color.ninja.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.color.ninja.MyColorNinja;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyColorNinja(), config);
+	}
+}
