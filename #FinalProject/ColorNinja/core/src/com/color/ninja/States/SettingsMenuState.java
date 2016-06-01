a030b19a0761fc98cccddef69a162bf4f5fdd6cd
zecst19 <zecst19@gmail.com>
2016-06-01 17:05:17 +0100

    oi

--- #FinalProject/ColorNinja/core/src/com/color/ninja/States/SettingsMenuState.java
+++ #FinalProject/ColorNinja/core/src/com/color/ninja/States/SettingsMenuState.java
@@ -1,9 +1,14 @@
 package com.color.ninja.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.color.ninja.MyColorNinja;
 
 /**
@@ -16,7 +21,9 @@ public class SettingsMenuState extends com.color.ninja.states.State {
     private Texture background;
 
     private Sprite title;
+    private Sprite BackBtnSprite;
 
+    private Button backBtn;
 
     public SettingsMenuState(com.color.ninja.states.GameStateManager gsm) {
 
@@ -24,13 +31,39 @@ public class SettingsMenuState extends com.color.ninja.states.State {
 
         background = new Texture("background.png");
         title = new Sprite(new Texture("title2.png"));
+        BackBtnSprite = new Sprite(new Texture ("backbtn.png"));
+
+        backBtn = new Button(new SpriteDrawable(BackBtnSprite));
 
         stage = new Stage();
+
 
         //title's position and size
         title.setSize(8 * MyColorNinja.WIDTH / 10, MyColorNinja.HEIGHT / 4);
         title.setCenterX(MyColorNinja.WIDTH / 2);
-        title.setY(MyColorNinja.HEIGHT - MyColorNinja.HEIGHT / 20 - title.getHeight());
+        title.setY(MyColorNinja.HEIGHT - MyColorNinja.HEIGHT / 9 - title.getHeight());
+
+        backBtn.setSize(MyColorNinja.WIDTH / 6, MyColorNinja.HEIGHT / 12);
+        backBtn.setX(MyColorNinja.WIDTH / 5 - backBtn.getWidth());
+        backBtn.setY(MyColorNinja.HEIGHT - MyColorNinja.HEIGHT / 10);
+
+        Gdx.input.setInputProcessor(stage);
+
+        stage.addActor(backBtn);
+
+        setListeners();
+
+    }
+
+    private void setListeners()
+    {
+        backBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gsm.set(new MainMenuState(gsm));
+            }
+        });
+
     }
 
 
@@ -52,6 +85,8 @@ public class SettingsMenuState extends com.color.ninja.states.State {
         sb.draw(background,0,0,MyColorNinja.WIDTH,MyColorNinja.HEIGHT);
 
         title.draw(sb);
+
+        backBtn.draw(sb,1);
 
         sb.end();   // Close batch.
 
