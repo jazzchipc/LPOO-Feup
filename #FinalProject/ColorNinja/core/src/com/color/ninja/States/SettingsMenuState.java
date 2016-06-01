2e3ba47915fe9e4e382737f67a2c5504570a9291
zecst19 <zecst19@gmail.com>
2016-06-01 19:31:06 +0100

    oi

--- #FinalProject/ColorNinja/core/src/com/color/ninja/States/SettingsMenuState.java
+++ #FinalProject/ColorNinja/core/src/com/color/ninja/States/SettingsMenuState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -11,6 +12,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.color.ninja.MyColorNinja;
@@ -27,37 +30,86 @@ public class SettingsMenuState extends com.color.ninja.states.State {
 
     private Sprite title;
     private Sprite BackBtnSprite;
+    private Sprite soundsVolSprite;
+    private Sprite musicVolSprite;
+    private Sprite difficultySprite;
 
     private Button backBtn;
 
     private Slider soundsVolSld;
+    private Slider musicVolSld;
+
+    private CheckBox easyCheckBox;
+    private CheckBox mediumCheckBox;
+    private CheckBox hardCheckBox;
+
+    int draggingPointer = -1;
 
     public SettingsMenuState(com.color.ninja.states.GameStateManager gsm) {
 
         super(gsm);
 
-       // skin = new Skin();
-       // skin.addRegions("ui/uiskin.atlas");
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
 
         background = new Texture("background.png");
         title = new Sprite(new Texture("title2.png"));
         BackBtnSprite = new Sprite(new Texture ("backbtn.png"));
+        soundsVolSprite = new Sprite(new Texture("soundsvolume.png"));
+        musicVolSprite = new Sprite(new Texture("musicvolume.png"));
+        difficultySprite = new Sprite(new Texture("difficulty.png"));
 
         backBtn = new Button(new SpriteDrawable(BackBtnSprite));
 
-       // soundsVolSld = new Slider(0,100,1,false,skin);
+        soundsVolSld = new Slider(0,100,1f,false,skin);
+        musicVolSld = new Slider(0,100,1f,false,skin);
+
+        easyCheckBox = new CheckBox("easy",skin);
+        mediumCheckBox = new CheckBox("medium",skin);
+        hardCheckBox = new CheckBox("hard",skin);
 
         stage = new Stage();
 
-
         //title's position and size
-        title.setSize(8 * MyColorNinja.WIDTH / 10, MyColorNinja.HEIGHT / 4);
+        title.setSize(8 * MyColorNinja.WIDTH / 10, MyColorNinja.HEIGHT / 5);
         title.setCenterX(MyColorNinja.WIDTH / 2);
         title.setY(MyColorNinja.HEIGHT - MyColorNinja.HEIGHT / 9 - title.getHeight());
 
         backBtn.setSize(MyColorNinja.WIDTH / 6, MyColorNinja.HEIGHT / 12);
         backBtn.setX(MyColorNinja.WIDTH / 5 - backBtn.getWidth());
         backBtn.setY(MyColorNinja.HEIGHT - MyColorNinja.HEIGHT / 10);
+
+        soundsVolSprite.setSize(MyColorNinja.WIDTH /2, MyColorNinja.HEIGHT / 15);
+        soundsVolSprite.setX(MyColorNinja.WIDTH / 2 - soundsVolSprite.getWidth()/2);
+        soundsVolSprite.setY(6* MyColorNinja.HEIGHT / 10 - soundsVolSprite.getHeight());
+
+        soundsVolSld.setSize(5*MyColorNinja.WIDTH /6 - soundsVolSld.getWidth()/2, MyColorNinja.HEIGHT /5);
+        soundsVolSld.setX(MyColorNinja.WIDTH / 2 - soundsVolSld.getWidth() / 2);
+        soundsVolSld.setY(2 * MyColorNinja.HEIGHT /5);
+
+        musicVolSprite.setSize(MyColorNinja.WIDTH /2, MyColorNinja.HEIGHT / 15);
+        musicVolSprite.setX(MyColorNinja.WIDTH / 2 - soundsVolSprite.getWidth()/2);
+        musicVolSprite.setY(9* MyColorNinja.HEIGHT / 20 - soundsVolSprite.getHeight());
+
+        musicVolSld.setSize(5*MyColorNinja.WIDTH /6 - musicVolSld.getWidth()/2, MyColorNinja.HEIGHT /5);
+        musicVolSld.setX(MyColorNinja.WIDTH / 2 - musicVolSld.getWidth() / 2);
+        musicVolSld.setY(2 * MyColorNinja.HEIGHT /8);
+
+        difficultySprite.setSize(MyColorNinja.WIDTH /2, MyColorNinja.HEIGHT / 15);
+        difficultySprite.setX(MyColorNinja.WIDTH / 2 - soundsVolSprite.getWidth()/2);
+        difficultySprite.setY(3* MyColorNinja.HEIGHT / 10 - soundsVolSprite.getHeight());
+
+        //easyCheckBox.setSize(MyColorNinja.WIDTH/10,MyColorNinja.HEIGHT/10);
+        easyCheckBox.setScale(2);
+        easyCheckBox.setX(MyColorNinja.WIDTH / 4 );
+        easyCheckBox.setY(3*MyColorNinja.HEIGHT /18);
+
+       // mediumCheckBox.setSize(MyColorNinja.WIDTH/5,MyColorNinja.HEIGHT/5);
+        mediumCheckBox.setX(MyColorNinja.WIDTH / 4 );
+        mediumCheckBox.setY(2*MyColorNinja.HEIGHT /18);
+
+        //hardCheckBox.setSize(MyColorNinja.WIDTH/10,MyColorNinja.HEIGHT/10);
+        hardCheckBox.setX(MyColorNinja.WIDTH / 4 );
+        hardCheckBox.setY(MyColorNinja.HEIGHT /18);
 
         Gdx.input.setInputProcessor(stage);
 
@@ -75,6 +127,13 @@ public class SettingsMenuState extends com.color.ninja.states.State {
                 gsm.set(new MainMenuState(gsm));
             }
         });
+
+        /*soundsVolSld.addListener(new ChangeListener(){
+            public void changed(ChangeEvent event, Actor actor) {
+                final int val = (int)soundsVolSld.getValue();
+
+            }
+        });*/
 
     }
 
@@ -100,7 +159,16 @@ public class SettingsMenuState extends com.color.ninja.states.State {
 
         backBtn.draw(sb,1);
 
-      //  soundsVolSld.draw(sb,1);
+        soundsVolSprite.draw(sb,1);
+        soundsVolSld.draw(sb,1);
+
+        musicVolSprite.draw(sb,1);
+        musicVolSld.draw(sb,1);
+
+        difficultySprite.draw(sb,1);
+        easyCheckBox.draw(sb,1);
+        mediumCheckBox.draw(sb,1);
+        hardCheckBox.draw(sb,1);
 
         sb.end();   // Close batch.
 
