fbf9166b1fc5b6b73345ebc3c79c7b8750781676
zecst19 <zecst19@gmail.com>
2016-06-06 16:21:36 +0100

    oi

---
+++ #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/GameTest.java
@@ -0,0 +1,12 @@
+package de.tomgrill.gdxtesting.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GameTest {
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(6, 6);
+    }
+}
---
+++ #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/InterfaceTest.java
@@ -0,0 +1,13 @@
+package de.tomgrill.gdxtesting.tests;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InterfaceTest {
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(5, 5);
+    }
+}
