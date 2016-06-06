bded3c931f84746ad0d97c27318e1c9d2d93884e
zecst19 <zecst19@gmail.com>
2016-06-06 16:28:45 +0100

    oi

--- #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/GameTest.java
+++ #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/GameTest.java
@@ -5,8 +5,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class GameTest {
+
     @Test
-    public void oneEqualsOne() {
+    public void clickShape(){
+        assertEquals(6, 6);
+    }
+
+    @Test
+    public void shapeIsDestroyed() {
+        assertEquals(6, 6);
+    }
+
+    @Test
+    public void shapeIsNotDestroyed(){
+        assertEquals(6, 6);
+    }
+
+    @Test
+    public void countdown(){
+        assertEquals(6, 6);
+    }
+
+    @Test
+    public void pauseGame(){
+        assertEquals(6, 6);
+    }
+
+    @Test
+    public void resumeGame(){
         assertEquals(6, 6);
     }
 }
--- #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/InterfaceTest.java
+++ #FinalProject/ColorNinja/tests/src/de/tomgrill/gdxtesting/tests/InterfaceTest.java
@@ -6,8 +6,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class InterfaceTest {
+
     @Test
-    public void oneEqualsOne() {
+    public void menuClick(){
+        assertEquals(5, 5);
+    }
+
+    @Test
+    public void noRotation() {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    public void screenSize(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void changeSoundsVol(){
+        assertEquals(5, 5);
+    }
+
+    @Test
+    public void changeMusicVol(){
+        assertEquals(5, 5);
+    }
+
+    @Test
+    public void musicIsPlaying(){
         assertEquals(5, 5);
     }
 }
